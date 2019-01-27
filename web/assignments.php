1034805a85b6fda139f14e5bfaaab23261f0d065
Cody Cornelison <codysmart@gmail.com>
2019-01-26 22:58:04 -0800

    Added week 3 assignment link

--- web/assignments.php
+++ web/assignments.php
@@ -1,6 +1,6 @@
 <?php include ("header.php"); ?>
 
 <h1>Cody Cornelison</h1>
-<a href="https://boiling-plateau-14805.herokuapp.com/"
+<a href="https://boiling-plateau-14805.herokuapp.com/">Week 3</a>
 
 <?php include ("footer.php"); ?>
